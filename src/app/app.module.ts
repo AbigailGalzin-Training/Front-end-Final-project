299d5750f413491cacbb1129946e7a78ac4221e0
Abigail Galzin <abigail.galzin@training.assuresoft.com>
2024-06-07 15:37:39 -0400

    files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainContentModule } from './main-content/main-content.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MainContentModule
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/app/main-content/main-content.module.ts
+++ src/app/main-content/main-content.module.ts
@@ -4,14 +4,21 @@ import { HeaderMusicComponent } from './header-music/header-music.component';
 import { NavigationMusicComponent } from './navigation-music/navigation-music.component';
 import { SideArtistComponent } from './side-artist/side-artist.component';
 import { AlbumContentComponent } from './album-content/album-content.component';
+import { MainContentComponent } from './main-content.component';
 
 @NgModule({
     declarations: [
+        MainContentComponent,
         HeaderMusicComponent,
         NavigationMusicComponent,
         SideArtistComponent,
-        AlbumContentComponent,
+        AlbumContentComponent
     ],
     imports: [CommonModule],
+    exports: [
+        MainContentComponent
+    ]
 })
-export class MainContentModule {}
+export class MainContentModule {
+    static forChild: any;
+}

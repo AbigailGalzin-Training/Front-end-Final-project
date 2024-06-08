536fb722497a02331d09434373374a6d09bc6ec1
Carolina Melendez <carolina.melendez@training.assuresoft.com>
2024-06-08 17:41:36 -0400

    fix: added spaces

--- src/app/main-content/side-artist/card-artist/card-artist.component.spec.ts
+++ src/app/main-content/side-artist/card-artist/card-artist.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CardArtistComponent } from './card-artist.component';
 
 describe('CardArtistComponent', () => {
-  let component: CardArtistComponent;
-  let fixture: ComponentFixture<CardArtistComponent>;
+    let component: CardArtistComponent;
+    let fixture: ComponentFixture<CardArtistComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CardArtistComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [CardArtistComponent]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(CardArtistComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(CardArtistComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });

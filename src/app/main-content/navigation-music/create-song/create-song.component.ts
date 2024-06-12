8516756443efd17056b8aa55e45ef4603aa03b99
Abigail Galzin <abigail.galzin@training.assuresoft.com>
2024-06-12 11:55:07 -0400

    modal song

--- src/app/main-content/navigation-music/create-song/create-song.component.ts
+++ src/app/main-content/navigation-music/create-song/create-song.component.ts
@@ -1,4 +1,5 @@
-import { Component, Inject } from '@angular/core';
+import { Component, EventEmitter, Inject, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -7,5 +8,50 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
     styleUrls: ['./create-song.component.sass']
 })
 export class CreateSongComponent {
-    constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+    createForm: FormGroup;
+    title!: string;
+    albums: any [] = [
+        {
+            title: 'Yellow',
+            genre: 'Rock',
+            releaseDate: '2000-06-26',
+            duration: 269,
+            songPath: 'https://example.com/music/yellow.mp3',
+        },
+        {
+            title: 'Shiver',
+            genre: 'Rock',
+            releaseDate: '2000-07-10',
+            duration: 299,
+            songPath: 'https://example.com/music/shiver.mp3',
+        },
+    ];
+    
+    @Output() close = new EventEmitter<void>();
+    @Output() save = new EventEmitter<any>();
+
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: any,
+        private fb: FormBuilder,
+    ) {
+        this.createForm = this.fb.group({
+            album: ['', Validators.required],
+            nameArtist: ['', Validators.required],
+            songTitle: ['', Validators.required],
+            songGenre: ['', Validators.required],
+            songYear: ['', Validators.required],
+            songDuration: ['', Validators.required],
+            link: ['', Validators.required],            
+        });
+    }
+
+    onSubmit() {
+        if (this.createForm.valid) {
+            this.save.emit(this.createForm.value);
+        }
+    }
+
+    closeModal() {
+        this.close.emit();
+    }
 }

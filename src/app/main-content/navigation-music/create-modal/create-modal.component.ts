09d0c07d59cd51307dc6fb485f7741edc8d41bf9
Abigail Galzin <abigail.galzin@training.assuresoft.com>
2024-06-12 10:33:59 -0400

    all modals title

--- src/app/main-content/navigation-music/create-modal/create-modal.component.ts
+++ src/app/main-content/navigation-music/create-modal/create-modal.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
     selector: 'app-create-modal',
@@ -7,10 +7,14 @@ import { MatDialogRef } from '@angular/material/dialog';
     styleUrls: ['./create-modal.component.sass'],
 })
 export class CreateModalComponent {
-    
+    title?: string;
+
     constructor(
-        public dialogRef: MatDialogRef<CreateModalComponent>
-    ) {}
+        public dialogRef: MatDialogRef<CreateModalComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: any
+    ) {
+        this.title = this.data.title;
+    }
 
     closeModal(): void {
         this.dialogRef.close();
--- src/app/main-content/navigation-music/navigation-music.component.ts
+++ src/app/main-content/navigation-music/navigation-music.component.ts
@@ -11,9 +11,15 @@ import { Router } from '@angular/router';
 export class NavigationMusicComponent {
     constructor(public dialog: MatDialog, private router: Router) { }
 
-    openCreateModal(type: string): void {
-        const dialogRef = this.dialog.open(CreateModalComponent);
-
+    openCreateModal(type: string, titleModal: string): void {
+        
+        const dialogRef = this.dialog.open(CreateModalComponent, {
+            data: {
+                type: type,
+                title: titleModal,
+            },
+        });
+        
         dialogRef.afterOpened().subscribe(() => {
             this.router.navigate([type]);
         });

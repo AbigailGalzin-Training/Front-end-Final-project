a9cfa2647aabdf81428da812a0d509443a8deb93
Carolina Melendez <carolina.melendez@training.assuresoft.com>
2024-06-13 14:42:19 -0400

    fix: deleted models folder

--- src/app/core/services/artist/artist.service.ts
+++ src/app/core/services/artist/artist.service.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Artist } from '../../../models/artist.model';
+import { Artist } from '../../../model/artist.model';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ArtistService {
     // TODO: until local storage is set
-    private artists: Artist[] = [
-        { id: 1, name: 'Artist One', photo: 'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/music/artists/profile/Taylor-Swift-fotor-jpg.jpg' },
-        { id: 2, name: 'Artist Two', photo: 'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-zion/zion/1667189514105-Maroon-wa_24c89811.jpeg' },
-        { id: 3, name: 'Artist Three', photo: 'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-zion/zion/1663268125697-WhatsApp_Image_2022-09-15_at_9.17.40_PM_(2).jpeg' },
-        { id: 4, name: 'Artist Four', photo: 'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-zion/zion/1666955170588-Ariana-Grande-wa_59e11327.jpeg' },
-        { id: 5, name: 'Artist Five', photo: 'https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-zion/zion/1672929736271-Selena_Gomez.jpg' },
-    ];
+    private artists: Artist[] = [];
     private storageKey = 'artistData';
 
     constructor() { }
@@ -26,5 +20,5 @@ export class ArtistService {
         storedData.push(data);
         localStorage.setItem(this.storageKey, JSON.stringify(storedData));
     }
-    
+
 }
